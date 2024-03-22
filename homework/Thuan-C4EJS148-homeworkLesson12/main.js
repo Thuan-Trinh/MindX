c7abd2146bf0fcfee85c9e69693981034e266d9f
Thuan-Trinh <thuantrinh7@gmail.com>
2024-03-22 19:06:20 +0700

    homework lesson 12

---
+++ homework/Thuan-C4EJS148-homeworkLesson12/main.js
@@ -0,0 +1,280 @@
+let products = [
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-7-1020x1020.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-8-1020x1020.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-9-1020x1020.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-10-1020x1020.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-11-1020x1020.jpg",
+        ],
+        nameProduct: "Artemis Lounge Chair",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "100000",
+        discount: "10",
+        SKU: "D2300-3-2-2",
+        category: "Dining Tables",
+        tags: "Trend",
+        status: "100"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-18.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-19.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-20.jpg",
+        ],
+        nameProduct: "Coby Coffee Table",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "150000",
+        discount: "7",
+        SKU: "D2300-3-2-1",
+        category: "Living Room",
+        tags: "Hot",
+        status: "50"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-30.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-31.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-32.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-33.jpg",
+        ],
+        nameProduct: "Della Chair – Navy",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "100000",
+        discount: "10",
+        SKU: "D2300-3",
+        category: "Living Room",
+        tags: "Trend",
+        status: "30"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-34.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-35.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-36.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-37.jpg",
+        ],
+        nameProduct: "Thin-leg table",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "21422",
+        discount: "0",
+        SKU: "D2300-2",
+        category: "Dining Tables",
+        tags: "Hot",
+        status: "50"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-83.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-84.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-85.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-86.jpg",
+        ],
+        nameProduct: "Luna Buffet",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "400000",
+        discount: "13",
+        SKU: "D1111",
+        category: "Bath Room",
+        tags: "Hot",
+        status: "20"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-79.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-80.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-81.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-82.jpg",
+        ],
+        nameProduct: "Liam Pendant Lighting",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "350000",
+        discount: "0",
+        SKU: "D1112",
+        category: "Lighting",
+        tags: "Hot",
+        status: "30"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2019/04/Image-13.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2019/04/Image-14.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2019/04/Image-15.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2019/04/Image-16.jpg",
+        ],
+        nameProduct: "Bend Goods Farmhouse Chair",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "350000",
+        discount: "12",
+        SKU: "U2022",
+        category: "Office",
+        tags: "Life Style",
+        status: "30"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-49.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-50.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-51.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-52.jpg",
+        ],
+        nameProduct: "Glass Dining Table",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "214000",
+        discount: "0",
+        SKU: "D2300",
+        category: "Office",
+        tags: "Hot",
+        status: "30"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/04/Image-91.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/04/Image-92.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/04/Image-93.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/04/Image-94.jpg",
+        ],
+        nameProduct: "Norwood Chenille Lounge Chair",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "500000",
+        discount: "8",
+        SKU: "U2009",
+        category: "Office",
+        tags: "",
+        status: "15"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-72.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-73.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-74.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-75.jpg",
+        ],
+        nameProduct: "LAX Series Entertainment Shelf",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "3500000",
+        discount: "2",
+        SKU: "D1114",
+        category: "Seating",
+        tags: "Trend",
+        status: "10"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-69.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-70.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-71.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-72.jpgs",
+        ],
+        nameProduct: "Long-leg Table",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "1200000",
+        discount: "",
+        SKU: " D1115",
+        category: "Seating",
+        tags: "",
+        status: "30"
+    },
+    {
+        img: [
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-41.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-42.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-43.jpg",
+            "https://wpbingosite.com/wordpress/funio/wp-content/uploads/2020/12/Image-43.jpg",
+        ],
+        nameProduct: "Fatboy Square Pillow",
+        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        price: "350000",
+        discount: "0",
+        SKU: "D1118",
+        category: "Seating",
+        tags: "",
+        status: "15"
+    }
+]
+
+
+let jsonProduct = JSON.stringify(products)  // chuyen tu mang ve string 
+localStorage.setItem('dataProduct', jsonProduct)  // luu vao loacal storage 
+
+// Thêm div ảnh từ local storage
+// Retrieve data from localStorage
+let Product = localStorage.getItem('dataProduct');
+let productsJson = JSON.parse(Product);
+
+// Function to get a random integer between min and max (inclusive)
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to select 6 random products
+function selectRandomProducts(array, count) {
+    let selectedProducts = [];
+    let shuffled = array.sort(() => 0.5 - Math.random());
+    let selected = shuffled.slice(0, count);
+    return selected;
+}
+
+// Select 6 random products
+let randomProducts = selectRandomProducts(productsJson, 6);
+
+// Function to filter products based on discount
+function filterProductsByDiscount(products, filterType) {
+    if (filterType === "none") {
+      return products.filter(product => product.discount === "0");
+    } else if (filterType === "lowToHigh") {
+      return products.slice().sort((a, b) => parseInt(a.discount) - parseInt(b.discount));
+    } else if (filterType === "highToLow") {
+      return products.slice().sort((a, b) => parseInt(b.discount) - parseInt(a.discount));
+    }
+  }
+  
+  // Function to render products
+  function renderProducts(products) {
+    document.getElementById('productContainer').innerHTML = ''; // Clear previous products
+    products.forEach(product => {
+      let randomImgIndex = getRandomInt(0, product.img.length - 1);
+      let randomImgUrl = product.img[randomImgIndex];
+  
+      // Create product element
+      let productElement = document.createElement('div');
+      productElement.classList.add('product');
+  
+      // Check if discount is 0% and conditionally display the discount tag
+      let discountTag = '';
+      if (product.discount !== "0") {
+        discountTag = `<span>${product.discount}%</span>`;
+      }
+  
+      // Add product details
+      productElement.innerHTML = `
+        <img src="${randomImgUrl}" alt="${product.nameProduct}">
+        <h3>${product.nameProduct}</h3>
+        <p>Price: ${product.price}</p>
+        ${discountTag}
+      `;
+  
+      // Append product element to the container
+      document.getElementById('productContainer').appendChild(productElement);
+    });
+  }
+  
+  // Render all products initially
+  renderProducts(randomProducts);
+  
+  // Add event listeners to filter buttons
+  document.getElementById('filterNone').addEventListener('click', function() {
+    let filteredProducts = filterProductsByDiscount(randomProducts, 'none');
+    renderProducts(filteredProducts);
+  });
+  
+  document.getElementById('filterLowToHigh').addEventListener('click', function() {
+    let filteredProducts = filterProductsByDiscount(randomProducts, 'lowToHigh');
+    renderProducts(filteredProducts);
+  });
+  
+  document.getElementById('filterHighToLow').addEventListener('click', function() {
+    let filteredProducts = filterProductsByDiscount(randomProducts, 'highToLow');
+    renderProducts(filteredProducts);
+  });
+  
